1ae0ceddceef9270dbdd27e51493a04ff62f7f52
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-04-13 16:33:34 -0300

    Subindo o final

--- PojetoEmpresa/Cadastrar.php
+++ PojetoEmpresa/Cadastrar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Exercício PHP POO</title>
+    <title>Exercício PHP </title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -17,18 +17,17 @@
             <label for="">Cidade: </label><br>
             <input type="text" id="cidade" name="cidade" value=""><br>
             <label for="">Salário por Hora: </label><br>
-            <input type="number" name="salHora" id="salHora" min="0" step=".01"><br>
+            <input type="text" name="salarioPorHora" id="salarioPorHora" min="0" step=".01"><br>
             <label for="">Horas Trabalhadas: </label><br>
-            <input type="number" name="horasTrab" id="horasTrab" min="0" step=".01"><br>
+            <input type="text" name="horasTrabalhadas" id="horasTrabalhadas" min="0" step=".01"><br>
             <label for="">Aumento: </label><br>
-            <input type="number" name="aumento" id="aumento" min="0" step=".01"><br><br>
+            <input type="text" name="aumento" id="aumento" min="0" step=".01"><br><br>
 
             <div class="container">
                 <input type="submit" class="btns" value="Cadastrar" name="btnCadastrar">
                 <input type="submit" class="btns" value="Calcular Salário" name="btnCalcular">
                 <input type="submit" class="btns" value="Mostrar Dados" name="btnMostrar">
                 <input type="submit" class="btns" value="Aumento Salarial" name="btnAumentar">
-                <b>asdp0ki</b>
             </div>
             
         </form>
@@ -39,45 +38,45 @@
 
 
 <?php 
-    require_once "Funcionario.php";
-    $funcionario = new Funcionario();
+    require_once "funcionario.php";
+    $funcionarioRecebe = new Funcionario();
     
     if(isset($_POST["btnCadastrar"]))
     {
-        $funcionario->setNome($_POST['nome']);
-        $funcionario->setCidade($_POST['cidade']);
-        $funcionario->setSalarioPorHora($_POST['salarioPorHora']);
-        $funcionario->setHorasTrabalhadas($_POST['horasTrabalhadas']);
-        $funcionario->setSalarioFinal($funcionario->CalcularSalario());
+        $funcionarioRecebe->setNome($_POST['nome']);
+        $funcionarioRecebe->setCidade($_POST['cidade']);
+        $funcionarioRecebe->setSalarioPorHora($_POST['salarioPorHora']);
+        $funcionarioRecebe->setHorasTrabalhadas($_POST['horasTrabalhadas']);
+        $funcionarioRecebe->setSalarioFinal($funcionarioRecebe->CalcularSalario());
 
         echo "<div class='container'><h3>Usuário Cadastrado com sucesso</h3><div>";
     }
 
     if(isset($_POST["btnCalcular"]))
     {
-        $funcionario->setSalarioPorHora($_POST['salarioPorHora']);
-        $funcionario->setHorasTrabalhadas($_POST['horasTrabalhadas']);
-        $salario = $funcionario->CalcularSalario();
+        $funcionarioRecebe->setSalarioPorHora($_POST['salarioPorHora']);
+        $funcionarioRecebe->setHorasTrabalhadas($_POST['horasTrabalhadas']);
+        $salario = $funcionarioRecebe->CalcularSalario();
         echo "<div class='container'><h3>Salário do Funcionário: <span id='salarioColor'> $salario</span></h3></div>";
     }
 
     if(isset($_POST["btnMostrar"]))
     {
-        $funcionario->setNome($_POST['nome']);
-        $funcionario->setCidade($_POST['cidade']);
-        $funcionario->setSalarioPorHora($_POST['salHora']);
-        $funcionario->setHorasTrabalhadas($_POST['horasTrabalhadas']);
-        $funcionario->setSalarioFinal($funcionario->CalcularSalario());
-        $dados = $funcionario->MostrarDados();
+        $funcionarioRecebe->setNome($_POST['nome']);
+        $funcionarioRecebe->setCidade($_POST['cidade']);
+        $funcionarioRecebe->setSalarioPorHora($_POST['salarioPorHora']);
+        $funcionarioRecebe->setHorasTrabalhadas($_POST['horasTrabalhadas']);
+        $funcionarioRecebe->setSalarioFinal($funcionarioRecebe->CalcularSalario());
+        $dados = $funcionarioRecebe->MostrarDados();
         echo "<div class='container'><h3>$dados<h3><div>";
     }
 
     if(isset($_POST["btnAumentar"]))
     {
-        $funcionario->setSalarioPorHora($_POST['salHora']);
-        $funcionario->setHorasTrabalhadas($_POST['horasTrab']);
-        $funcionario->setSalarioFinal($funcionario->CalcularSalario());
-        $aumento = $funcionario->DarAumentoAoFuncionario($_POST['aumento']);
+        $funcionarioRecebe->setSalarioPorHora($_POST['salarioPorHora']);
+        $funcionarioRecebe->setHorasTrabalhadas($_POST['horasTrabalhadas']);
+        $funcionarioRecebe->setSalarioFinal($funcionarioRecebe->CalcularSalario());
+        $aumento = $funcionarioRecebe->DarAumentoAoFuncionario($_POST['aumento']);
         echo "<div class='container'><h3>Salário com aumento: <span id='salarioColor'>R$  $aumento foi o aumento dado</span><h3><div>";
     }
 
--- PojetoEmpresa/funcionario.php
+++ PojetoEmpresa/funcionario.php
@@ -9,64 +9,65 @@ class Funcionario
     //gets e sets Nome
     public function getNome()
     {
-        return $this->$nome;
+        return $this->nome;
 
     }
     public function setNome($receberNome)
     {
-        $this->$nome = $receberNome;
+        $this->nome = $receberNome;
     }
     //get set Cidade
     public function getCidade()
     {
-       return $this->$cidade;
+       return $this->cidade;
     }
     public function setCidade($receberCidade)
     {
-        $this->$cidade = $receberCidade;
+        $this->cidade = $receberCidade;
     }
     //get e sets Salario por hora
     public function getSalarioPorHora()
     {
-        return $this->$salarioPorHora;
+        return $this->salarioPorHora;
     }
     public function setSalarioPorHora($receberSalarioPorHora)
     {
-        $this->$salarioPorHora = $receberSalarioPorHora;
+        $this->salarioPorHora = $receberSalarioPorHora;
     }
     //get e set das horas trabalhadas
     public function getHorasTrabalhadas()
     {
-       return $this->$horasTrabalhadas;
+       return $this->horasTrabalhadas;
     }
     public function setHorasTrabalhadas($receberHorasTrbalhadas)
     {
-        $this->$horasTrabalhadas = $receberHorasTrbalhadas;
+        $this->horasTrabalhadas = $receberHorasTrbalhadas;
     }
     //get e set Salario Final
     public function getSalarioFinal()
     {
-        return $this->$salarioFinal;
+        return $this->salarioFinal;
     }
     public function setSalarioFinal($receberSalarioFinal)
     {
-        $this->$salarioFinal = $receberSalarioFinal;
+        $this->salarioFinal = $receberSalarioFinal;
     }
     //Metodos
     public function CalcularSalario()
     {
-        $this->$salarioFinal = $this->$horasTrabalhadas * $this->$salarioPorHora * 30;
-        return $this->$salarioFinal;
+        $this->salarioFinal = $this->horasTrabalhadas * $this->salarioPorHora * 30;
+        return $this->salarioFinal;
     }
     public function MostrarDados()
     {
-        "Seu nome é : ".$nome. " " "Sua cidade é: " .$cidade. " " "Seu salario por horas trabalhadas são: " .$horasTrabalhadas.
-        " " "Suas horas trabalhadas são: " .$horasTrabalhadas. " " " E por fim seu salario final é :".$salarioFinal.
+        return "Seu nome é :  " .$this->nome." Sua cidade é: " .$this->cidade." Seu salario por horas trabalhadas são: ".$this->horasTrabalhadas.
+        " Suas horas trabalhadas são: ".$this->horasTrabalhadas." E por fim seu salario final é: ".$this->salarioFinal;
+        
     }
     public function DarAumentoAoFuncionario($aumento)
     {
-        $this->$salarioFinal+=$aumento;
-        return $this->$salarioFinal;
+        $this->salarioFinal+=$aumento;
+        return $this->salarioFinal;
     }
     
 }
